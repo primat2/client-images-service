2f939578c703bd2e9ac520a8f4b5d5570a5efaf9
artem <cerebrum90@gmail.com>
2026-03-31 22:40:53 +0500

    init project

---
+++ src/main/kotlin/ru/orangegt/client_images_service/ClientImagesServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.orangegt.client_images_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientImagesServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientImagesServiceApplication>(*args)
+}
